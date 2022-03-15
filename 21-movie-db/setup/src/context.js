a952ef742f9e208a3aa6d32aa9151c8896e5d35c
godwin-nj <amadigodwin7@gmail.com>
2022-03-15 12:17:38 +0100

    movie omdb tut

--- 21-movie-db/setup/src/context.js
+++ 21-movie-db/setup/src/context.js
@@ -1,14 +1,47 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-const AppContext = React.createContext()
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+// console.log(API_ENDPOINT);
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [query, setQuery] = useState("wolverine");
+
+  const fetchMovies = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: `${data.Error}` });
+      }
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError({ show: true, msg: "Error fetching API" });
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+  return (
+    <AppContext.Provider value={{ loading, movies, error, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
